1c1ead7a3400dee3f3c0174369523970173b24db
James Rose <jamesbrose123@gmail.com>
2013-05-26 19:57:42 +0100

    Cross-compiled is hard-coded into the makefile so don't alert if we're not using it.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,15 +3,9 @@
 #endif
 #include <stddef.h>
 #include <stdint.h>
- 
-/* Check if the compiler thinks if we are targeting the wrong operating system. */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
- 
+
 /* Hardware text mode color constants. */
-enum vga_color
-{
+enum vga_color {
   COLOR_BLACK = 0,
   COLOR_BLUE = 1,
   COLOR_GREEN = 2,
@@ -30,20 +24,17 @@ enum vga_color
   COLOR_WHITE = 15,
 };
  
-uint8_t make_color(enum vga_color fg, enum vga_color bg)
-{
+uint8_t make_color(enum vga_color fg, enum vga_color bg) {
   return fg | bg << 4;
 }
  
-uint16_t make_vgaentry(char c, uint8_t color)
-{
+uint16_t make_vgaentry(char c, uint8_t color) {
   uint16_t c16 = c;
   uint16_t color16 = color;
   return c16 | color16 << 8;
 }
  
-size_t strlen(const char* str)
-{
+size_t strlen(const char* str) {
   size_t ret = 0;
   while ( str[ret] != 0 )
     ret++;
@@ -58,8 +49,7 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
  
-void terminal_initialize()
-{
+void terminal_initialize() {
   terminal_row = 0;
   terminal_column = 0;
   terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
@@ -72,19 +62,16 @@ void terminal_initialize()
   }
 }
  
-void terminal_setcolor(uint8_t color)
-{
+void terminal_setcolor(uint8_t color) {
   terminal_color = color;
 }
  
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
-{
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
   terminal_buffer[index] = make_vgaentry(c, color);
 }
  
-void terminal_putchar(char c)
-{
+void terminal_putchar(char c) {
   if(c == '\n')
   {
     ++terminal_row;
@@ -100,15 +87,13 @@ void terminal_putchar(char c)
   }
 }
  
-void terminal_writestring(const char* data)
-{
+void terminal_writestring(const char* data) {
   size_t datalen = strlen(data);
   for ( size_t i = 0; i < datalen; i++ )
     terminal_putchar(data[i]);
 }
 
-void kernel_main()
-{
+void kernel_main() {
   terminal_initialize();
   terminal_writestring("Hello, James Rose!\nWhat's up?");
 }
