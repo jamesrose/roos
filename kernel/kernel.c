45ab470f923ea1aeb3ba5c8def2769f0ad3d68d6
James Rose <jamesbrose123@gmail.com>
2013-05-27 23:15:43 +0100

    Include stdbool.h regardless of compiler.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,9 @@
-#if !defined(__cplusplus)
-#include <stdbool.h> /* C doesn't have booleans by default. */
-#endif
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
- 
+
 /* Hardware text mode color constants. */
-enum vga_color
-{
+enum vga_color {
   COLOR_BLACK = 0,
   COLOR_BLUE = 1,
   COLOR_GREEN = 2,
@@ -25,20 +22,17 @@ enum vga_color
   COLOR_WHITE = 15,
 };
  
-uint8_t make_color(enum vga_color fg, enum vga_color bg)
-{
+uint8_t make_color(enum vga_color fg, enum vga_color bg) {
   return fg | bg << 4;
 }
  
-uint16_t make_vgaentry(char c, uint8_t color)
-{
+uint16_t make_vgaentry(char c, uint8_t color) {
   uint16_t c16 = c;
   uint16_t color16 = color;
   return c16 | color16 << 8;
 }
  
-size_t strlen(const char* str)
-{
+size_t strlen(const char* str) {
   size_t ret = 0;
   while ( str[ret] != 0 )
     ret++;
@@ -53,8 +47,7 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
  
-void terminal_initialize()
-{
+void terminal_initialize() {
   terminal_row = 0;
   terminal_column = 0;
   terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
@@ -67,19 +60,16 @@ void terminal_initialize()
   }
 }
  
-void terminal_setcolor(uint8_t color)
-{
+void terminal_setcolor(uint8_t color) {
   terminal_color = color;
 }
  
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
-{
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
   terminal_buffer[index] = make_vgaentry(c, color);
 }
  
-void terminal_putchar(char c)
-{
+void terminal_putchar(char c) {
   if(c == '\n')
   {
     ++terminal_row;
@@ -94,16 +84,14 @@ void terminal_putchar(char c)
     }
   }
 }
- 
-void terminal_writestring(const char* data)
-{
+
+void terminal_writestring(const char* data) {
   size_t datalen = strlen(data);
   for ( size_t i = 0; i < datalen; i++ )
     terminal_putchar(data[i]);
 }
 
-void kernel_main()
-{
+void kernel_main() {
   terminal_initialize();
   terminal_writestring("Hello, James Rose!\nWhat's up?");
 }
