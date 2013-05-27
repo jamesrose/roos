2d88ae616da15b41390f0554e67a8428c02bcdb7
James Rose <jamesbrose123@gmail.com>
2013-05-27 23:53:56 +0100

    Clean up Makefile. Move terminal stuff out of kernel.c into terminal.c file

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
+OBJECTS = build/boot.o build/kernel.o build/terminal.o
+
 default: clean all run
 
 all: build/kernel
 
-kernel/boot.o: kernel/boot.asm
+build/boot.o: kernel/boot.asm
 	nasm -felf kernel/boot.asm -o build/boot.o
 
-kernel/kernel.o: kernel/kernel.c
-	i586-elf-gcc -c kernel/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+build/%.o: kernel/%.c
+	i586-elf-gcc -c $< -o $@ -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
-build/kernel: kernel/boot.o kernel/kernel.o
-	i586-elf-gcc -T kernel/linker.ld -o build/kernel -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o
+build/kernel: $(OBJECTS)
+	i586-elf-gcc -T kernel/linker.ld -o build/kernel -ffreestanding -O2 -nostdlib $(OBJECTS)
 
 clean:
-	rm -f -v *.o *.bin
+	rm -f -v build/*
 
 run:
 	qemu-system-i386 -kernel build/kernel
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,25 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* Hardware text mode color constants. */
-enum vga_color {
-  COLOR_BLACK = 0,
-  COLOR_BLUE = 1,
-  COLOR_GREEN = 2,
-  COLOR_CYAN = 3,
-  COLOR_RED = 4,
-  COLOR_MAGENTA = 5,
-  COLOR_BROWN = 6,
-  COLOR_LIGHT_GREY = 7,
-  COLOR_DARK_GREY = 8,
-  COLOR_LIGHT_BLUE = 9,
-  COLOR_LIGHT_GREEN = 10,
-  COLOR_LIGHT_CYAN = 11,
-  COLOR_LIGHT_RED = 12,
-  COLOR_LIGHT_MAGENTA = 13,
-  COLOR_LIGHT_BROWN = 14,
-  COLOR_WHITE = 15,
-};
+#include "kernel.h"
  
 uint8_t make_color(enum vga_color fg, enum vga_color bg) {
   return fg | bg << 4;
@@ -38,65 +20,8 @@ size_t strlen(const char* str) {
     ret++;
   return ret;
 }
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 24;
- 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_reset() {
-  for ( size_t y = 0; y < VGA_HEIGHT; y++ ) {
-    for ( size_t x = 0; x < VGA_WIDTH; x++ ) {
-      const size_t index = y * VGA_WIDTH + x;
-      terminal_buffer[index] = make_vgaentry(' ', terminal_color);
-    }
-  }
-}
-
-void terminal_initialize() {
-  terminal_row = 0;
-  terminal_column = 0;
-  terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
-  terminal_buffer = (uint16_t*) 0xB8000;
-  terminal_reset();
-}
- 
-void terminal_setcolor(uint8_t color) {
-  terminal_color = color;
-}
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
-  const size_t index = y * VGA_WIDTH + x;
-  terminal_buffer[index] = make_vgaentry(c, color);
-}
- 
-void terminal_putchar(char c) {
-  if(c == '\n')
-  {
-    ++terminal_row;
-    terminal_column = 0;
-  } else {
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if ( ++terminal_column == VGA_WIDTH ) {
-      terminal_column = 0;
-      if ( ++terminal_row == VGA_HEIGHT ) {
-        terminal_row = 0;
-      }
-    }
-  }
-}
-
-void terminal_writestring(const char* data) {
-  size_t datalen = strlen(data);
-  for ( size_t i = 0; i < datalen; i++ )
-    terminal_putchar(data[i]);
-}
 
 void kernel_main() {
   terminal_initialize();
-  terminal_writestring("Hello, James Rose!\nWhat's up?");
-  terminal_reset();
+  terminal_writestring("Hello, James Rose!\nWhat's up?");  
 }
